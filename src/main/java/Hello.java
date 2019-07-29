fe9f0a5ac57105a1c96b22e5510099becccbc1c6
Niklay94 <niklaywow@yandex.ru>
2019-07-29 17:49:54 +0300

    Новая ветка + добавление даты

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,9 @@
+import java.util.Date;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("И снова привет мир!!!, x2");
+        Date date = new Date();
+        System.out.println("Дата сегодня: "+date.toString());
     }
 }
