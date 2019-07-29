23e0628d6611489b2c59e728e3c75cc73f76a1dd
Niklay94 <niklaywow@yandex.ru>
2019-07-29 18:11:39 +0300

    Новая ветка + выводим любое послание в мир

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,8 +2,6 @@ import java.util.Date;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("И снова привет мир!!!, x2");
-        Date date = new Date();
-        System.out.println("Дата сегодня: "+date.toString());
+        System.out.println("Выводим любое послание в мир");
     }
 }
