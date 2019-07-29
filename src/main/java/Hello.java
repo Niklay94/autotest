055f83b55b8c1079c02398591b575a6dbb1015a5
Niklay94 <niklaywow@yandex.ru>
2019-07-29 17:33:54 +0300

    Привет мир х2

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("И снова привет мир!!!");
+        System.out.println("И снова привет мир!!!, x2");
     }
 }
