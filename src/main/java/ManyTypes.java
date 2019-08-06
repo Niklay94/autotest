dae5e5fbd965b0cb1ac5ea6d7befe6564529566b
Niklay94 <niklaywow@yandex.ru>
2019-08-06 16:04:29 +0300

    модуль2 лекция2 задание №1

---
+++ src/main/java/ManyTypes.java
@@ -0,0 +1,14 @@
+public class ManyTypes {
+
+    boolean tumbler;
+    long a0 = -9223372036854775808L;
+    short a1 = 12345;
+    Character a2 = new Character('5');
+    int a3 = 2147483647;
+    long a4 = 9223372036854775807L;
+    Boolean a5 = new Boolean("Infinity");
+    Double a6 = new Double("-Infinity");
+    String someName;
+    Object mail;
+
+}
--- src/main/java/Point.java
+++
@@ -1,7 +0,0 @@
-public class Point {
-
-    int x;
-
-    int y;
-
-}
